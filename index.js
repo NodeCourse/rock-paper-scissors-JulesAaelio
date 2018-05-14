32ccac90894f3ac27bfe8042a25892e322f48907
Jules LAURENT <jules.laurent@ynov.com>
2018-05-14 11:50:02 +0200

    hello world with pug

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 const Express = require('express');
+const app = new Express();
+app.set('view engine', 'pug');
+
 console.log('App started at',Date.now());
+app.use((req, res) => {
+    res.render('choose')
+});
+app.listen(3000);
